0a281db8c872035f403a8fb906ad847a803c4283
ENDY-NGUYEN\Dell's <trishoang2309@gmail.com>
2023-11-13 11:29:42 +0700

    Bai tap them student vao list

--- src/main/java/com/cybersoft/demospringboot03/controller/HelloController.java
+++ src/main/java/com/cybersoft/demospringboot03/controller/HelloController.java
@@ -1,6 +1,4 @@
-package com.cybersoft.demospringboot03.controller;
-
-/*
+package com.cybersoft.demospringboot03.controller;/*
 * Để định nghĩa đường dẫn thì ta có 2 cái annotation
 * @Controller: dùng để định nghĩa đường dẫn khi nội dung của đường dẫn trả ra là 1 trang html
 * @ResponseBody: Giúp Controller có thể trả ra kiểu String  dùng để viết API
@@ -8,22 +6,57 @@ package com.cybersoft.demospringboot03.controller;
 * chuỗi API
 * */
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cybersoft.demospringboot03.Entity.Student;
+import com.cybersoft.demospringboot03.payload.request.LoginRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    // Tham số truyền trên trình duyệt: @RequestParam
+    // Tham số truyền ngầm: @RequestParam
+    // Tham số đóng vai tr như là 1 đường dẫn: @PathVariable
+    // Tham số ngầm là đối tượng: @RequestBody
 
-    @GetMapping("")
-    public String hello(){
+    List<Student> studentList = new ArrayList<Student>();
+    @GetMapping("/student")
+    public List<Student> getStudentList(){
 
-        return "Hello Spring Boot";
+        return studentList;
     }
 
-    @GetMapping("/cybersoft")
-    public String helloCyberSoft(){
-        return "Hello CyberSoft";
+    @PostMapping("/student/add")
+    public List<Student> getStudentList(@RequestParam String name, @RequestParam int age){
+        Student student = new Student(name,age);
+        studentList.add(student);
+        return studentList;
     }
+
+    @PostMapping("/student/{name}/{age}")
+    public ResponseEntity<?> getAllStudentList(@PathVariable String name, @PathVariable int age){
+        try {
+            Student student = new Student(name,age);
+            studentList.add(student);
+            return new ResponseEntity<>("Add success " + studentList ,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("Error occurred while adding student.", HttpStatus.OK);
+        }
+
+    }
+
+    @PostMapping("/student/add/v1")
+    public ResponseEntity<?> getListStudent(@RequestBody Student student){
+        try {
+            studentList.add(student);
+            return new ResponseEntity<>("Add success " + studentList, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("Error occurred while adding student.", HttpStatus.OK);
+        }
+    }
+
 }
